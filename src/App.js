b26fce8b3ea9d5ab9101e51b2368730fea19ea0e
Gagandeep Singh <g2mgs4@gmail.com>
2021-08-07 16:25:10 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React,{useState} from 'react'
+import './style.css'
+const App = () => {
+    const [value,setValue] = useState('')
+    const [data,setData] = useState('')
+    const getValues = async ()=>{
+        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${value}&appid=7c7c180a9582489ed9b78155e63b8d28&units=metric`)
+        if(response.status==200){
+        const data = await response.json()
+        setData(data.main.temp)
+        }else{
+            setData("No results")
+        }
+    }
+    
+    return (
+        <div>
+            <input value={value} onChange={(e)=>setValue(e.target.value)}></input>
+            <h1>{value}</h1>
+            <button onClick={getValues}>Get</button>
+            <h1>{data}</h1>
+        </div>
+    )
 }
 
-export default App;
+export default App
