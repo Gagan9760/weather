c08f23b9e18af205bc33af9f1212ec855dc443c5
Gagandeep Singh <g2mgs4@gmail.com>
2021-08-08 13:29:37 +0530

    papersdark

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React, { useState } from 'react'
 import './style.css'
 import { Button, TextField, Grid,Paper, Typography } from '@material-ui/core'
 import Nav from './Nav'
+import { ThemeProvider,createTheme } from '@material-ui/core/styles';
+
 
 
 const App = () => {
+    const [dark, setDark] = useState(false)
+    const theme = createTheme({
+        palette:{
+            type:dark?"dark":"light"
+        }
+    })
+
+
     const [value, setValue] = useState('')
     const [disdata, setData] = useState({})
-    const [dark, setDark] = useState(false)
+    
     const getValues = async () => {
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=7c7c180a9582489ed9b78155e63b8d28&units=metric`)
         if (response.status === 200) {
@@ -25,15 +35,16 @@ const App = () => {
             setData("No results")
         }
     }
-    const theme = () => {
-        setDark(!dark)
-    }
+    // const theme = () => {
+    //     setDark(!dark)
+    // }
 
     return (
-        <>
+        <ThemeProvider theme={theme}>
+            <Paper>
             <Grid className="con" container>
                 <Grid className="item" item xs={12} >
-                    <Nav />
+                    <Nav dark={dark} setDark={setDark}/>
                 </Grid>
                 <Grid className="item" item xs={0} sm={3} />
                 <Grid className="item2"  item xs={12} sm={6}>
@@ -42,7 +53,7 @@ const App = () => {
                         <TextField variant="outlined" label="Enter city name" value={value} onChange={(e) => setValue(e.target.value)}></TextField>
                         <Typography variant="h4" align="center">{value}</Typography>
                         <Button variant="contained" style={{ margin: "10px" }} color="primary" onClick={getValues}>Get</Button>
-                        <Button variant="contained" style={{ margin: "10px" }} onClick={theme} color="secondary">{dark ? "Light Mode" : "Dark Mode"}</Button>
+                        {/* <Button variant="contained" style={{ margin: "10px" }} onClick={theme} color="secondary">{dark ? "Light Mode" : "Dark Mode"}</Button> */}
                         </div>
                         <Grid container spacing={2}>
                         <Grid item xs={2}/>
@@ -60,8 +71,11 @@ const App = () => {
                         <Grid item xs={5}>
                         {disdata.temp && 
                         <Paper className="minmax">
+                        <Typography variant="h6" align="center">
+                            Min:
+                        </Typography>
                         <Typography variant="h5" align="center">
-                            {`Min: ${disdata.min} °C`}
+                            {`${disdata.min} °C`}
                         </Typography>
                         </Paper>}
                         </Grid>
@@ -69,7 +83,10 @@ const App = () => {
                             {disdata.temp &&
                         <Paper className="minmax">
                         <Typography variant="h5" align="center">
-                            {`Max: ${disdata.max} °C`}
+                        Max:
+                        </Typography>
+                        <Typography variant="h5" align="center">
+                            {`${disdata.max} °C`}
                         </Typography>
                         </Paper>}
                         </Grid>
@@ -90,8 +107,8 @@ const App = () => {
                 </Grid>
                 <Grid className="item" item xs={0} sm={3} />
             </Grid>
-
-        </>
+            </Paper>
+        </ThemeProvider>
     )
 }
 
--- src/Nav.js
+++ src/Nav.js
@@ -1,13 +1,33 @@
 import React from 'react'
-import { AppBar,Typography } from '@material-ui/core'
-const Nav = () => {
+import { AppBar, Typography, Toolbar, Grid } from '@material-ui/core'
+import CloudOutlinedIcon from '@material-ui/icons/CloudOutlined';
+import Switch from '@material-ui/core/Switch';
+
+const Nav = ({dark,setDark}) => {
     return (
         <div>
-            <AppBar color="secondary" position="static" style={{top:0}}>
-            <Typography variant="h5" style={{width:"100%",textAlign:'center',padding:'10px  '}}>
-            Weather
-          </Typography>
+
+            <AppBar color="secondary" position="static" style={{ top: 0 }}>
+                
+                <Grid container >
+                    
+                    <Grid item xs={12} style={{
+                        display:"grid",
+                        placeItems:"center"
+                    }} >
+                    <Toolbar>
+                        <Typography variant="h5" align="center" >
+                            Weather
+                        </Typography>
+                        <CloudOutlinedIcon />
+                        <Switch color="primary" checked={dark} onChange={()=>setDark(!dark)}></Switch>
+                    </Toolbar>
+                    </Grid>
+                    
+                </Grid>
+                
             </AppBar>
+
         </div>
     )
 }
