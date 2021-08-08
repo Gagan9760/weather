e335bb640a434340a33048c1d381d47a91557fe7
Gagandeep Singh <g2mgs4@gmail.com>
2021-08-08 18:10:16 +0530

    framer

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import React, { useState } from 'react'
 import './style.css'
-import {Grid,Paper} from '@material-ui/core'
+import { Grid, Paper } from '@material-ui/core'
 import Nav from './Nav'
-import { ThemeProvider,createTheme,makeStyles } from '@material-ui/core/styles';
+import { ThemeProvider, createTheme } from '@material-ui/core/styles';
 import Input from './Input';
+import { motion } from "framer-motion";
 
-// const useStyles = makeStyles({
-//     paper:{
-//         backgroundColor: "#2B6BD6"
-        
-//     }
-// })
 
 const App = () => {
-    // const classes = useStyles();
+
     const [dark, setDark] = useState(false)
     const theme = createTheme({
-        palette:{
-            type:dark?"dark":"light",
-            primary:{
-                main:"#2B6BD6"
+        palette: {
+            type: dark ? "dark" : "light",
+            primary: {
+                main: "#2B6BD6"
             },
-            secondary:{
-                main:"#1B419C"
+            secondary: {
+                main: "#1B419C"
             }
         }
     })
@@ -30,7 +25,7 @@ const App = () => {
 
     const [value, setValue] = useState('')
     const [disdata, setData] = useState({})
-    
+
     const getValues = async () => {
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=7c7c180a9582489ed9b78155e63b8d28&units=metric`)
         if (response.status === 200) {
@@ -50,17 +45,42 @@ const App = () => {
     }
     return (
         <ThemeProvider theme={theme}>
-            <Paper className={disdata.type==="Thunderstorm"||disdata.type==="Rain"||disdata.type==="Drizzle"?"conrain":(disdata.type==="Clouds"?"concloud":(disdata.type==="Clear"?"consunny":"con"))}>
-            <Grid container>
-                <Grid className="item" item xs={12} >
-                    <Nav dark={dark} setDark={setDark}/>
+            <Paper className={disdata.type === "Thunderstorm" || disdata.type === "Rain" || disdata.type === "Drizzle" ? "conrain" : (disdata.type === "Clouds" ? "concloud" : (disdata.type === "Clear" ? "consunny" : "con"))}>
+                <Grid container>
+                    <Grid
+                        component={motion.nav}
+                        initial={{
+                            y: -50,
+                            opacity: 0
+                        }}
+                        animate={{
+                            y: 0,
+                            opacity: 1
+                        }}
+                        transition={{
+                            type: "tween"
+                        }} className="item" item xs={12} >
+                        <Nav dark={dark} setDark={setDark} />
+                    </Grid>
+                    <Grid className="item" item xs={0} sm={3} />
+                    <Grid className="item2" 
+                    component={motion.div} 
+                    initial={{ 
+                        x: -200, 
+                        opacity: 0 
+                        }} 
+                    animate={{ 
+                        x: 0, 
+                        opacity: 1 
+                    }} 
+                    transition={{
+                        delay: 0.5
+                    }}
+                    item xs={12} sm={6}>
+                        <Input value={value} disdata={disdata} setValue={setValue} getValues={getValues} />
+                    </Grid>
+                    <Grid className="item" item xs={0} sm={3} />
                 </Grid>
-                <Grid className="item" item xs={0} sm={3} />
-                <Grid className="item2"  item xs={12} sm={6}>
-                <Input value={value} disdata={disdata} setValue={setValue} getValues={getValues}/>
-                </Grid>
-                <Grid className="item" item xs={0} sm={3} />
-            </Grid>
             </Paper>
         </ThemeProvider>
     )
--- src/Input.js
+++ src/Input.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import { Button, TextField,Paper, Typography } from '@material-ui/core'
 import Data from './Data'
+import { motion } from "framer-motion";
+
+
 const Input = ({value,disdata,setValue,getValues}) => {
     return (
         <>
             <Paper className="pap" elevation={10}>
                         <div className="paper">
-                        <TextField variant="outlined" label="Enter city name" value={value} onChange={(e) => setValue(e.target.value)}></TextField>
+                        <TextField
+                        component={motion.h1}
+                        initial={{y:-100,opacity:0}}
+                        animate={{y:0,opacity:1}}
+                        variant="outlined" label="Enter city name" value={value} onChange={(e) => setValue(e.target.value)}></TextField>
                         <Typography variant="h4" align="center">{value}</Typography>
-                        <Button variant="contained" style={{ margin: "10px" }} color="primary" onClick={getValues}>Get</Button>
+                        <Button
+                        component={motion.div}
+                        initial={{y:-50,opacity:0}}
+                        animate={{y:0,opacity:1,transition:{delay:1}}}
+                        whileTap={{scale:0.9}}
+                        whileHover={{scale:1.1}}
+                        variant="contained" style={{ margin: "10px" }} color="primary" onClick={getValues}>Get</Button>
                         </div>
                         <Data disdata={disdata} />
                     
